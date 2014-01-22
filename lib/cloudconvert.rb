b88a27a9bdb599ceea04e03f15f3978f59436c1c
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2014-01-22 23:15:11 +0400

    refactoring...

--- lib/cloudconvert.rb
+++ lib/cloudconvert.rb
@@ -1,2 +1,11 @@
 require 'cloudconvert/version'
+require 'cloudconvert/configuration'
 require 'cloudconvert/session'
+
+module Cloudconvert
+  CLOUDCONVERT_URL = "https://api.cloudconvert.org"
+  API_KEY_ERROR = "API key is not configured"
+  CONNECTION_ERROR = "Response code was not HTTP:OK"
+
+  Cloudconvert.configure
+end
--- lib/cloudconvert/session.rb
+++ lib/cloudconvert/session.rb
@@ -6,64 +6,67 @@ require "openssl"
 
 module Cloudconvert
   class Session 
-    attr_accessor :cloudconvert_uri
-  
-    def initialize
-      config = YAML.load_file('cloudconvert.yml')
-      @cloudconvert_api_key = config['api_key']
+    attr_accessor :cloudconvert_uri, :input_format, :output_format
+
+    def initialize(input_format, output_format)
+      res = Net::HTTP.post_form(URI.parse(CLOUDCONVERT_URL + "/process"), 
+        'inputformat' => input_format.to_s, 
+        'outputformat' => output_format.to_s, 
+        'apikey' => Session.api_key)
+
+      raise_error_if_res_code_not_200(res)
+
+      @cloudconvert_uri = URI("https:" + JSON.parse(res.body)["url"])
+      @input_format = input_format
+      @output_format = output_format
     end
-  
-    def start(inputformat, outputformat)
-      resp = Net::HTTP.post_form(
-        URI.parse('https://api.cloudconvert.org/process'),
-        'inputformat' => inputformat.to_s, 'outputformat' => outputformat, 'apikey' => @cloudconvert_api_key)
-  
-      @cloudconvert_uri = URI("https:" + JSON.parse(resp.body)["url"])
-      @input_format = inputformat
-      @output_format = outputformat
-    end
-  
+    
     def upload(file_path)
-      File.open(file_path, "rb") do |file|
-        req = Net::HTTP::Post::Multipart.new @cloudconvert_uri.path + "?outputformat=#{@output_format}&email=1",
-          "file" => UploadIO.new(file, "text/plain", file_path)
-  
-        res = cloudconvert_http.start do |http|
-          http.request(req)
-        end
+      File.open(file_path, "rb") { |file| upload_file(file, file_path) }        
+    end
+
+    def upload_file(file, file_path)
+      req = Net::HTTP::Post::Multipart.new @cloudconvert_uri.path + "?outputformat=#{@output_format}",
+        "file" => UploadIO.new(file, "text/plain", file_path)
+
+      res = cloudconvert_http.start do |http|
+        http.request(req)
       end
+      raise_error_if_res_code_not_200(res)
     end
   
     def status
-      resp = cloudconvert_http.start do |http|
+      res = cloudconvert_http.start do |http|
         req = Net::HTTP::Get.new @cloudconvert_uri
         http.request(req)
       end
+      raise_error_if_res_code_not_200(res)
   
-      JSON.parse(resp.body)
+      JSON.parse(res.body)
     end
-  
-    def wait_until_complete_and_download_to(result_filepath = nil, debug_mode = false)
+
+    def wait_until_complete_and
       current_status = {}
       begin
         current_status = status()
-        puts current_status.to_s if debug_mode
-        current_percents = current_status["percent"].to_f
-        current_phase = current_status["step"].to_s
-      end until ((current_phase == "finished") and (current_percents == 100.0))
+        # current_percents = current_status["percent"].to_f
+      end until (current_status["step"].to_s == "finished")
+      # and (current_percents == 100.0)
+
+      yield(current_status)
+    end
   
-      download_uri = URI("https:" + current_status["output"]["url"])
-      result_filepath ||= current_status["output"]["filename"]
-  
-      cloudconvert_http(download_uri).start do |http|
-        req = Net::HTTP::Get.new download_uri
-        resp = http.request(req)
-        open(result_filepath, "wb") do |file|
-          file.write(resp.body)
+    def wait_until_complete_and_download
+      wait_until_complete_and do |status|
+        download_uri = URI("https:" + status["output"]["url"])
+        cloudconvert_http(download_uri).start do |http|
+          req = Net::HTTP::Get.new download_uri
+          res = http.request(req)
+          raise_error_if_res_code_not_200(res)
+          yield(res.body)
         end
       end
-  
-      puts "Successfully downloaded and saved file: #{result_filepath}!"
+
     end
   
   private
@@ -72,6 +75,15 @@ module Cloudconvert
       http.use_ssl = uri.port == 443
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE if http.use_ssl?
       return http
+    end
+
+    def raise_error_if_res_code_not_200(res)
+      raise "#{CONNECTION_ERROR}: #{res.to_s}" unless res.code == "200"
+    end
+
+    def self.api_key
+      raise API_KEY_ERROR if Cloudconvert.configuration.api_key.nil?
+      Cloudconvert.configuration.api_key
     end
   end
 end
